d8e4d78b80eff3ec51bc827eecd715dece8e102a
Stuart Stein <qmstuart@gmail.com>
2021-05-03 20:06:30 +0100

    Added conditional repeats tests

--- src/test/interpreter-tests.cljs
+++ src/test/interpreter-tests.cljs
@@ -1,6 +1,6 @@
 (ns test.interpreter-tests
   (:require [cljs.test :refer [deftest is testing run-tests]]
-            [exfn.interpreter :refer [build-symbol-table interpret]]))
+            [exfn.interpreter :refer [interpret]]))
 
 (deftest build-symbol-table-tests
   (is (= {:foo 2, :bar 4, :quax 5}
@@ -20,9 +20,53 @@
                               [:nop]]))))
 
 
-(deftest interpreter-tests
-  (testing "Simple mov instruction is interpreted correctly."
-    (is (= {:registers {:a 5}}
-           (int)))))
+(deftest conditional-repeats 
+  (testing "rgz repeats"
+    (let [memory {:registers {:a -1} :eip 2 :eip-stack [0]}
+          result (interpret [[:rep] [:nop] [:rgz :a]] memory)]
+      (is (and (= (-> result :memory :eip ) 1)
+               (= (-> result :memory :eip-stack) [0])))))
+  
+  (testing "rgz does not repeat"
+    (let [memory {:registers {:a 1} :eip 2 :eip-stack [0]}
+          result (interpret [[:rep] [:nop] [:rgz :a]] memory)]
+      (is (and (= (-> result :memory :eip) 3)
+               (= (-> result :memory :eip-stack) [])))))
+  
+  (testing "rlz repeats"
+    (let [memory {:registers {:a 0} :eip 2 :eip-stack [0]}
+          result (interpret [[:rep] [:nop] [:rlz :a]] memory)]
+      (is (and (= (-> result :memory :eip) 1)
+               (= (-> result :memory :eip-stack) [0])))))
+  
+  (testing "rlz does not repeat"
+    (let [memory {:registers {:a -1} :eip 2 :eip-stack [0]}
+          result (interpret [[:rep] [:nop] [:rlz :a]] memory)]
+      (is (and (= (-> result :memory :eip) 3)
+               (= (-> result :memory :eip-stack) [])))))
+  
+  (testing "rlez does not repeat"
+    (let [memory {:registers {:a 1} :eip 2 :eip-stack [0]}
+          result (interpret [[:rep] [:nop] [:rlez :a]] memory)]
+      (is (and (= (-> result :memory :eip) 1)
+               (= (-> result :memory :eip-stack) [0])))))
+  
+  (testing "rlez repeats"
+    (let [memory {:registers {:a 0} :eip 2 :eip-stack [0]}
+          result (interpret [[:rep] [:nop] [:rlez :a]] memory)]
+      (is (and (= (-> result :memory :eip) 3)
+               (= (-> result :memory :eip-stack) [])))))
+  
+  (testing "rgez does not repeats"
+    (let [memory {:registers {:a 0} :eip 2 :eip-stack [0]}
+          result (interpret [[:rep] [:nop] [:rgez :a]] memory)]
+      (is (and (= (-> result :memory :eip) 3)
+               (= (-> result :memory :eip-stack) [])))))
+  
+  (testing "rgez repeats"
+    (let [memory {:registers {:a -1} :eip 2 :eip-stack [0]}
+          result (interpret [[:rep] [:nop] [:rgez :a]] memory)]
+      (is (and (= (-> result :memory :eip) 1)
+               (= (-> result :memory :eip-stack) [0]))))))
 
 (run-tests)
