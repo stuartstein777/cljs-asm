392775509f75c2a5fc1f2e0377a0dfc49a944ee8
Stuart Stein <qmstuart@gmail.com>
2021-05-13 00:19:34 +0100

    More work on parser and comments

--- src/main/exfn/validators.cljs
+++ src/main/exfn/validators.cljs
@@ -3,6 +3,11 @@
             [exfn.helpers :as h]
             [exfn.errors :as err]))
 
+;;=================================================================================================================
+;; validate-instruction
+;;
+;; runs an instructions ruleset against the supplied args and collects the error(s).
+;;=================================================================================================================
 (defn validate-instruction [instr args is-macro?]
   (let [{:keys [macro-applicable non-macro-applicable]} (err/rules instr)
         applied  (map #(partial % is-macro?) macro-applicable)
@@ -12,18 +17,39 @@
               (when error
                 (str "Invalid `" instr "` call, `" instr "` " error)))) all-rules)))
 
+;;=================================================================================================================
+;; get-section
+;;
+;; maps the source . sections to a keyword.
+;;=================================================================================================================
 (defn get-section [code]
   (condp = code
     ".data" :data
     ".code" :code
     ".macros" :macro))
 
+;;=================================================================================================================
+;; join
+;;
+;; joins a number of strings in the form:
+;;  foo, bar or quax
+;;  foo or bar
+;;  foo
+;;=================================================================================================================
 (defn join [suggestions]
   (condp = (count suggestions)
     1 (first suggestions)
     2 (str (first suggestions) " or " (second suggestions))
     (str (str/join ", " (butlast suggestions)) " or " (last suggestions))))
 
+;;=================================================================================================================
+;; build-error
+;;
+;; Takes an invalid instruction, the line number, is-macro? determining if this is a macro-call or not, and the
+;; possible replacements, for regular instructions these will be the valid instructions. If it's a macro call then
+;; the replacements would be parsed macros.
+;; If we have more than 4 suggestions then don't suggest anything as this means we didn't really match on anything.
+;;=================================================================================================================
 (defn build-error [invalid-instruction line-no replacements is-macro?]
   (let [suggestions (h/get-suggestions-for-invalid-instruction invalid-instruction replacements)]
     (if (> (count suggestions) 4)
@@ -67,17 +93,30 @@
       (re-find #"\w+:" code)
       acc
 
+      ;; else its a regular instruction (but may be an instruction in a macro):
       :else
       (if (not (h/valid-instructions instruction))
+        ; if its invalid instruction, then add error.
         (update acc :errors conj (build-error instruction line-no h/valid-instructions false))
+        ;else, validate the instruction has the correct arguments and types.
         (let [errors (->> (validate-instruction instruction args open-macro)
                           (map #(str line-no ": " %)))]
+          ; if we got any errors add them to the errors key. Otherwise just return the acc unchanged.
           (if (seq errors)
             (update acc :errors #(apply conj %1 %2) errors)
             acc))))))
 
 ;;=================================================================================================================
+;; verify
 ;;
+;; This is the reducing function for the verifier.
+;;
+;; It accumulates a current parsing state.
+;; If we hit a .macro, .code or .data  line then this means we are parsing macros, so the accumulator flag
+;; :current-section is set.
+;; If we hit a macro definition then we need to set the :open-macro flag to true.
+;; If we hit an end macro definition then we need to se the :open-macro flag to false. etc
+;; See the comments at each cond for more details.
 ;;=================================================================================================================
 (defn verify [{:keys [current-section open-macro macro-names] :as acc} [line-no code]]
   (cond
@@ -85,7 +124,6 @@
     (or (= code ".macros") (= code ".code") (= code ".data"))
     (assoc acc :current-section (get-section code))
 
-    ;; macro parsing --------------------------------------------------------------------------------------
     ;; TODO: Can move all this out to a macro parse function.
     ; if we are in a macro, and we hit a %end line, then close the current macro.
     (and (= :macro current-section) (= code "%end") open-macro)
@@ -124,56 +162,54 @@
     (= :code current-section)
     (validate-line acc line-no code macro-names)
 
-    ;; code parsing ---------------------------------------------------------------------------------------
-
     :else
     acc))
 
 
 
-(def sample-code '(".macros"
-                   "%square-and-sum"
-                   "mul 5 %1"
-                   "muk %2 %2"
-                   "add %1 %2"
-                   "%end"
-                   "%add-ten"
-                   "add %1 10"
-                   "%end"
-                   ".code"
-                   "mov :a 0"
-                   "mov :b 1"
-                   "squareandsub(:a, :b)"
-                   "mov :c 2"
-                   "prn :b"
-                   "call foo"
-                   "mul :c"
-                   "cmp :a :b"
-                   "ffne quax"
-                   "mul :c 10"
-                   "quax:"
-                   "nop"
-                   "cdlk bar"
-                   "pop :d"
-                   "pop :e"
-                   "prn :d"
-                   "prn :e"
-                   "xor :b :b"
-                   "end" "foo:"
-                   "inc :b"
-                   "xxx"
-                   "ret"
-                   "bar:"
-                   "add :a 7"
-                   "sub :c 1"
-                   "push 3"
-                   "push 4"
-                   "wet"
-                   ".data"
-                   "xyz 123"))
+(comment (def sample-code '(".macros"
+                            "%square-and-sum"
+                            "mul 5 %1"
+                            "muk %2 %2"
+                            "add %1 %2"
+                            "%end"
+                            "%add-ten"
+                            "add %1 10"
+                            "%end"
+                            ".code"
+                            "mov :a 0"
+                            "mov :b 1"
+                            "squareandsub(:a, :b)"
+                            "mov :c 2"
+                            "prn :b"
+                            "call foo"
+                            "mul :c"
+                            "cmp :a :b"
+                            "ffne quax"
+                            "mul :c 10"
+                            "quax:"
+                            "nop"
+                            "cdlk bar"
+                            "pop :d"
+                            "pop :e"
+                            "prn :d"
+                            "prn :e"
+                            "xor :b :b"
+                            "end" "foo:"
+                            "inc :b"
+                            "xxx"
+                            "ret"
+                            "bar:"
+                            "add :a 7"
+                            "sub :c 1"
+                            "push 3"
+                            "push 4"
+                            "wet"
+                            ".data"
+                            "xyz 123")))
+
 
 (defn validate [code]
-  ;; want line numbers with each line.
   (->> (map vector (range (count sample-code)) code)
        (reduce verify {:current-section nil
                        :errors          []
